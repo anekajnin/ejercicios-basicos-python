30868fb204a5d1ac301535e9ae7f0cc7f5365339
anekajnin <55347325+anekajnin@users.noreply.github.com>
2020-01-05 00:22:39 +0100

    Ejercicios 10 y 11 (bucles)

---
+++ 10-while.py
@@ -0,0 +1,12 @@
+contador = 0
+
+while contador < 10:
+    print("Dentro del bucle")
+    contador +=1
+    
+    if contador == 6:
+        break
+else:
+    print('El contador ha terminado') #else opcional
+    
+#Palabra reservada break (salir del bucle) continue (continuar)
---
+++ 11-for.py
@@ -0,0 +1,27 @@
+#Lista
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+for valor in lista:
+    print(valor)
+    
+#Tupla
+lista = (1,2,3,4,5,6,7,8,9,10)
+
+for valor in lista:
+    print(valor)
+    
+print("\n\n")
+
+nuevo_rango = range(0,10)
+for valor in nuevo_rango:
+    print(valor)
+    
+print("\n\n")
+    
+nuevo_rango = range(0,20,2)
+for valor in nuevo_rango:
+    print(valor)
+    
+nuevo_rango = range(10) #De 0 a 9
+for valor in nuevo_rango:
+    print(valor)
